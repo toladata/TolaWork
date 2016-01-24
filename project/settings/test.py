e8406d41fb055d64bef3085f690b0b1710023054
Greg Lind <gwlind@gmail.com>
2016-01-24 11:54:19 -0800

    update requirements to remove python-mysql module for now

--- project/settings/test.py
+++ project/settings/test.py
@@ -1,10 +1,6 @@
 from base import *
 
-########## TEST SETTINGS
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
-TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
-TEST_DISCOVER_ROOT = SITE_ROOT
-TEST_DISCOVER_PATTERN = "test_*.py"
+
 ########## IN-MEMORY TEST DATABASE
 DATABASES = {
     "default": {
@@ -16,3 +12,75 @@ DATABASES = {
         "PORT": "",
     },
 }
+"""Development settings and globals."""
+
+from os.path import join, normpath
+
+
+#from mongoengine import connect
+
+########## MANAGER CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
+ADMINS = (
+    ('test', 'test@test.com'),
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
+MANAGERS = ADMINS
+########## END MANAGER CONFIGURATION
+
+
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+########## END EMAIL CONFIGURATION
+
+########## EMAIL SETTINGS
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'test@test.com'
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'test@test.com'
+SERVER_EMAIL = "test@test.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#DEFAULT_TO_EMAIL = 'to email'
+
+########## END EMAIL SETTINGS
+
+
+########## MongoDB Connect
+
+#connect('feeds')
+
+########## END DATABASE CONFIGURATION
+
+########## GOOGLE CLIENT CONFIG ###########
+GOOGLE_STEP2_URI = ''
+GOOGLE_CLIENT_ID = ''
+GOOGLE_CLIENT_SECRET = ''
+
+
+########## CACHE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+########## END CACHE CONFIGURATION
+
+
+
+
+
