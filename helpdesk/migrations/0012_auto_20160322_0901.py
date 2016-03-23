b5b249ba992ee2cf4de849361dec032267cec214
joash <amembaj@gmail.com>
2016-03-23 21:24:27 +0300

    fix migrations with kb

--- helpdesk/migrations/0012_auto_20160322_0901.py
+++ helpdesk/migrations/0012_auto_20160322_0901.py
@@ -11,23 +11,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='SuperKBCategory',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('title', models.CharField(max_length=100, verbose_name='Title')),
-                ('slug', models.SlugField(verbose_name='Slug')),
-                ('description', models.TextField(verbose_name='Description')),
-            ],
-            options={
-                'ordering': ['title'],
-                'verbose_name': 'Knowledge base supercategory',
-                'verbose_name_plural': 'Knowledge base supercategories',
-            },
-        ),
-        migrations.AlterModelOptions(
-            name='kbcategory',
-            options={'ordering': ['title'], 'verbose_name': 'KnCatowledge base category', 'verbose_name_plural': 'Knowledge base categories'},
-        ),
+       
+
 
     ]
