e873a1c370275e83d3f3543b526c854e6a1664cc
gichiraanne <gichiraanne@gmail.com>
2016-05-30 16:49:42 +0300

    the enhancement of the search interface on the ticket page

--- helpdesk/views/staff.py
+++ helpdesk/views/staff.py
@@ -746,7 +746,7 @@ def ticket_list(request):
         ticket_qs = apply_query(tickets, query_params)
 
 
-    ticket_paginator = paginator.Paginator(ticket_qs, 4)
+    ticket_paginator = paginator.Paginator(ticket_qs, 5)
     try:
         page = int(request.GET.get('page', '1'))
     except ValueError:
