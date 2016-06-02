654724a6db2117e0c15e31471803a52f8635da41
faithnekesa <faynekesa@gmail.com>
2016-06-02 16:10:24 +0300

    update on the task model and added the task follow-up models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from tasks.models import Task
+from tasks.models import Task, TaskFollowUp, TaskChange
 # Register your models here.
 class TasksAdmin(admin.ModelAdmin):
     list_display = ('task', 'priority', 'due_date')
-
+class TaskChangeInline(admin.StackedInline):
+    model = TaskChange
+class TaskFollowUpAdmin(admin.ModelAdmin):
+    inlines = [TaskChangeInline]
+    list_display = ('task', 'date', 'task_name', 'comment')
+    list_display_links = ('task',)
 admin.site.register(Task)
+admin.site.register(TaskFollowUp, TaskFollowUpAdmin)
