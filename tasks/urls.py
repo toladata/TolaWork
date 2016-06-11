79f1f0124b115d5e710492932d68e22a0eedd63a
odenypeter <odenypeter@gmail.com>
2016-06-11 12:32:35 +0300

    save task from the form in modal

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = patterns('tasks.views',
+
     url(r'^admin/', admin.site.urls),
+
     url(r'^$', views.task_list, name='task_list'),
+
+    url(r'^tasks/submit/$',
+        'create_task',
+        name='task_submit'),
 )
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,46 @@
 from django.shortcuts import render, render_to_response
+from django.template import RequestContext, loader, Context
+from django.contrib.auth.models import User
 from tasks.models import Task
+from datetime import datetime
 # Create your views here.
 def task_list(request):
-    task = Task.objects.all()
+
+    tasks = Task.objects.all()
+    assignable_users = User.objects.filter(is_active=True).order_by(User.USERNAME_FIELD)
+    created_by = ''
+    if request.user.is_authenticated():
+    	created_by = request.user
 
     return render(request, 'tasks/task_index.html', {
-        'task': task,
+        'tasks': tasks,
+        'assignable_users': assignable_users,
+        'created_by': created_by,
 
         })
+
+def create_task(request):
+	if request.method == 'POST':
+		title = request.POST.get('title')
+		submitter_mail = request.POST.get('submitter_mail')
+		status = request.POST.get('status')
+		priority = request.POST.get('priority')
+		due_date = datetime.strptime(request.POST.get('due_date'), "%Y-%m-%d %H:%M:%S")
+		completed_date = datetime.strptime(request.POST.get('completed_date'),"%Y-%m-%d %H:%M:%S")
+		created_by = request.POST.get('created_by')
+		assigned_to = request.POST.get('assigned_to')
+		note = request.POST.get('note')
+
+		task = Task(task=title, submitter_email=submitter_mail, status=status, priority=priority, due_date=due_date, completed_date=completed_date,created_by_id=created_by, assigned_to_id=assigned_to, note=note)
+		task.save()
+
+		print (task)
+	tasks = Task.objects.all()
+
+	return render_to_response('tasks/task_index.html',
+        RequestContext(request, {
+        	'tasks':tasks,
+            
+        }))
+
+
