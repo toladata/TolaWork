0694c0ffe77f905794d2223a72da9ac052739482
gichiraanne <gichiraanne@gmail.com>
2016-08-03 13:54:41 +0300

    Change in the default number of tickets on the tickets page

--- helpdesk/views/staff.py
+++ helpdesk/views/staff.py
@@ -1203,7 +1203,7 @@ def ticket_list(request):
         status__in=[Ticket.CLOSED_STATUS, Ticket.RESOLVED_STATUS])
 
 
-    tickets = Ticket.objects.select_related()
+    tickets = Ticket.objects.all()
     num_tickets = tickets.count()
 
     queue_choices = Queue.objects.all()
@@ -1236,7 +1236,7 @@ def ticket_list(request):
         ticket_qs = apply_query(tickets, query_params)
 
     #Change items per_page by a user
-    items_per_page = 2
+    items_per_page = 10
     user_choice_pageItems = request.GET.get('items_per_page')
 
     if user_choice_pageItems:
