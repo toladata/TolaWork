28ba3b80047196cd4e481fd84fddd71f72bdf813
gichiraanne <gichiraanne@gmail.com>
2016-08-23 14:58:29 +0300

    Highlighting of keywords after a search in the ticket list page

--- project/settings/base.py
+++ project/settings/base.py
@@ -223,6 +223,7 @@ THIRD_PARTY_APPS = (
     'tinymce',
     'easy_select2',
     'endless_pagination',
+    'haystack',
 
 
 )
@@ -356,3 +357,12 @@ ENDLESS_PAGINATION_PER_PAGE = 8
 ENDLESS_PAGINATION_TEMPLATE_VARNAME = 'template'
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+    },
+}
+HAYSTACK_CUSTOM_HIGHLIGHTER = 'helpdesk.lib.MyHighlighter'
