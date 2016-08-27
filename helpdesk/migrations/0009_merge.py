aa1c6548bc1b985107687606f8ca1e60b0ba673e
joash <amembaj@gmail.com>
2016-08-27 12:20:28 +0300

    merge migration

---
+++ helpdesk/migrations/0009_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('helpdesk', '0006_auto_20160811_1410'),
+        ('helpdesk', '0008_auto_20160815_1211'),
+        ('helpdesk', '0008_merge'),
+    ]
+
+    operations = [
+    ]
