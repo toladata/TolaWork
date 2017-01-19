0ae751c5ad857e2e8a912606f030bef7df8d2c80
odenypeter <odenypeter@gmail.com>
2017-01-19 15:38:03 +0300

    github sync error

--- helpdesk/github.py
+++ helpdesk/github.py
@@ -61,7 +61,7 @@ def new_issue(repo,ticket):
     ticket_comments = FollowUp.objects.filter(ticket_id=ticket.id).all()
     new_comment = ''
     for t in ticket_comments:
-        comments = new_comment + str(t.comment) + '<br>'
+        comments = new_comment + unicode(t.comment).encode('utf-8') + '<br>'
         new_comment = unicode(comments).encode('utf-8')
     attachment_note = ''
     ticket_attachments = FollowUp.objects.filter(ticket_id = ticket.id).prefetch_related('attachment_set')
