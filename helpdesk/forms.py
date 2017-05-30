3c7d1454eb97613dc17d1301cc4d9b6bb206e32c
odenypeter <odenypeter@gmail.com>
2017-05-30 09:33:03 +0300

    more fixes

--- helpdesk/forms.py
+++ helpdesk/forms.py
@@ -222,14 +222,14 @@ class TicketForm(forms.Form):
 
         q = Queue.objects.get(id=int(self.cleaned_data['queue']))
         q = Queue.objects.get(id=int(self.cleaned_data['queue']))
-        org = Organization.objects.get(id=1)
+        #org = Organization.objects.get(id=1)
 
         t = Ticket( title = self.cleaned_data['title'],
                     submitter_email = self.cleaned_data['submitter_email'],
                     created = timezone.now(),
                     status = Ticket.OPEN_STATUS,
                     queue = q,
-                    organization = org,
+                    #organization = org,
                     description = self.cleaned_data['body'],
                     error_msg = self.cleaned_data['error_msg'],
                     priority = self.cleaned_data['priority'],
