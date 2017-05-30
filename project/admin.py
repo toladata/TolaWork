38f337fbb3068f93888527df0fe777613d94d019
odenypeter <odenypeter@gmail.com>
2017-05-30 09:07:04 +0300

    add organization to admin site

--- project/admin.py
+++ project/admin.py
@@ -7,6 +7,10 @@ from models import *
 class TolaUserAdmin(admin.ModelAdmin):
 	list_filter = ( 'activity_url',)
 
+class OrganizationAdmin(admin.ModelAdmin):
+	list_filter = ( 'name',)
+
 admin.site.register(TolaUser, TolaUserAdmin)
+admin.site.register(Organization, OrganizationAdmin)
 
 admin.site.register(Announcement)
