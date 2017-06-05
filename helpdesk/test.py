1759d27020d3bcbbed9f76acd4ec3a4bdf82035f
odenypeter <odenypeter@gmail.com>
2017-06-05 13:56:51 +0300

    add Ticket model unit test

--- helpdesk/test.py
+++ helpdesk/test.py
@@ -56,6 +56,7 @@ class QueueTestCase(TestCase):
         self.assertEqual(Queue.objects.filter(id=get_queue.id).count(), 1)
 
 class TagTestCase(TestCase):
+
     def setUp(self):
         new_tag = Tag.objects.create(name="TestCaseTag")
         new_tag.save()
@@ -63,6 +64,32 @@ class TagTestCase(TestCase):
     def test_tag_exists(self):
         get_tag = Tag.objects.get(name="TestCaseTag")
         self.assertEqual(Tag.objects.filter(id=get_tag.id).count(), 1)
+
+class TicketTestCase(TestCase):
+
+    def setUp(self):
+        user = User.objects.create_user('doe', 'doe@open.test', 'doepassword')
+        user.save()
+        get_user = User.objects.get(email="doe@open.test");
+
+        queue = Queue.objects.create(title="TestQueue1", slug="TestQueue1")
+        queue.save()
+        get_queue = Queue.objects.get(title="TestQueue1")
+
+        organization = Organization.objects.create(name="OpenBuild", description="Test OpenBuild description", organization_url="http://open.build")
+        organization.save()
+        get_organization = Organization.objects.get(name="OpenBuild")
+
+        new_ticket = Ticket.objects.create(title="Just a TestCase Ticket", queue=get_queue, organization=get_organization, submitter_email=get_user.email)
+        new_ticket.save()
+
+    def test_ticket_exists(self):
+        get_ticket = Ticket.objects.get(title="Just a TestCase Ticket")
+        self.assertEqual(Ticket.objects.filter(id=get_ticket.id).count(), 1)
+
+
+
+        
         
 
         
