3eeb5c1ebfde4af2face0f23a8b9eb8961a526ab
odenypeter <odenypeter@gmail.com>
2017-06-05 12:24:31 +0300

    add announcement model unit test

--- project/test.py
+++ project/test.py
@@ -1,28 +1,23 @@
 from django.test import TestCase
 from django.test import RequestFactory
 from django.test import Client
-from helpdesk.models import DocumentationApp
+from project.models import *
 from django.contrib.auth.models import User
-
-
-class DocumentationAppTestCase(TestCase):
-
-    def setUp(self):
-        user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
-        user.save()
-        new_documentation = DocumentationApp.objects.create(name="testdoc", documentation="test the documentation")
-        new_documentation.save()
-
-    def test_documentation_exists(self):
-        """Check for Documentation object"""
-        get_documentation = DocumentationApp.objects.get(name="testdoc")
-        self.assertEqual(DocumentationApp.objects.filter(id=get_documentation.id).count(), 1)
 
 class OrganizationTestCase(TestCase):
     def setUp(self):
         new_organization = Organization.objects.create(name="OpenBuild", description="Test OpenBuild description", organization_url="http://open.build")
         new_organization.save()
 
-    def organization_exists(self):
+    def test_organization_exists(self):
         get_organization = Organization.objects.get(name="OpenBuild")
-        self.assertEqual(Organizatio.objects.filter(id=get_organization.id).count(), 1)
+        self.assertEqual(Organization.objects.filter(id=get_organization.id).count(), 1)
+
+class AnnouncementTestCase(TestCase):
+	def setUp(self):
+		new_announcement = Announcement.objects.create(title="Test Announcement", description="Test Announcement description")
+		new_announcement.save()
+
+	def test_announcement_exists(self):
+		get_announcement = Announcement.objects.get(title="Test Announcement")
+		self.assertEqual(Announcement.objects.filter(id=get_announcement.id).count(), 1)
